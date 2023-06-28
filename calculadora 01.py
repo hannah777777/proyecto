cf4785072271b9f6d99a451e382be7378a567d0c
hannah777777 <138017382+hannah777777@users.noreply.github.com>
2023-06-28 09:37:49 -0400

    Add files via upload

---
+++ calculadora 01.py
@@ -0,0 +1,18 @@
+# titulo del programa
+print("*"60)
+print("calculadora01")
+print("*"60)
+
+
+numero1 = float(input("ingresar primer numero"))
+numero2 = float(input("ingresar el segundo numero"))
+
+
+
+print("la suma es:",numero1+numero2)
+
+# este input matiene abierto el programa
+input()
+
+if __name__ == '__main__':
+	sys.exit(main())
